7e4dd339085e32acf0b582a26c03762b5b8c027c
naophis <nao1288stusj@gmail.com>
2022-01-05 05:34:16 +0900

    matlab対応3

---
+++ tools/matlab_ws/model/gen_code/rtGetNaN.h
@@ -0,0 +1,20 @@
+#ifndef RTW_HEADER_rtGetNaN_h_
+#define RTW_HEADER_rtGetNaN_h_
+#include <stddef.h>
+#include "rtwtypes.h"
+#include "rt_nonfinite.h"
+#ifdef __cplusplus
+
+extern "C" {
+
+#endif
+
+  extern real_T rtGetNaN(void);
+  extern real32_T rtGetNaNF(void);
+
+#ifdef __cplusplus
+
+}
+#endif
+#endif
+
