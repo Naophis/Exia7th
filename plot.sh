2ebb59a13c310d6cc89eac2c27a695f659a0ec40
Naophis <nao1288stusj@gmail.com>
2022-02-13 14:52:16 +0900

    過去のplotを描画できるように修正

--- plot.sh
+++ plot.sh
@@ -1,3 +1,24 @@
 
-rosrun plotjuggler plotjuggler -d ./tools/param_tuner/logs/latest.csv -l ./tools/param_tuner/profile.xml
+if [ $# -ne 0 ];then
+    idx=$1
+    if expr "$idx" : "[0-9]*$" >&/dev/null; then
+        offset_idx=$(($idx+2))
 
+        i=0
+        for file in `ls -rt ./tools/param_tuner/logs/ | tail -n ${offset_idx}`
+            do
+            echo $file $i ${offset_idx}
+            if [ $i -eq 0 ]; then
+                echo ./tools/param_tuner/logs/$file
+                `rosrun plotjuggler plotjuggler -d ./tools/param_tuner/logs/$file -l ./tools/param_tuner/profile.xml`
+                break
+            fi
+            i=$(($i+1))
+        done
+    else
+        echo "not number"
+    fi
+
+else
+    rosrun plotjuggler plotjuggler -d ./tools/param_tuner/logs/latest.csv -l ./tools/param_tuner/profile.xml
+fi
