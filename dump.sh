868e376877b0a59d87fb2b70ca0c3034fefd1567
Naophis <nao1288stusj@gmail.com>
2023-01-05 14:49:14 +0900

    bug fix

--- dump.sh
+++ dump.sh
@@ -2,5 +2,5 @@
 
 echo thread apply all bt full
 
-espcoredump.py --chip esp32s3 -p /dev/ttyUSB1 dbg_corefile build/main.elf
+espcoredump.py --chip esp32s3 -p /dev/ttyUSB0 dbg_corefile build/main.elf
 # espcoredump.py --chip esp32s3 -p /dev/ttyUSB1 info_corefile -t b64  ./build/main.elf
